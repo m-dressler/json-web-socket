a011d408cbe176cb0966dad942c09db148decb91
Mauritz <mdressler@prodport.com>
2022-07-28 14:39:35 +0200

    Enabled handle of onopen onclose and onerror

--- JsonWebSocket.js
+++ JsonWebSocket.js
@@ -1,8 +1,18 @@
 const JsonWebSocket = (wsUrl) => {
-  const ws = new WebSocket(wsUrl);
+  const public = {};
 
   const EVENT_LISTENERS = {};
 
+  const ws = new WebSocket(wsUrl);
+  ws.onopen = () => {
+    if (public.onopen) public.onopen();
+  };
+  ws.onclose = () => {
+    if (public.onclose) public.onclose();
+  };
+  ws.onerror = () => {
+    if (public.onerror) public.onerror();
+  };
   ws.onmessage = (message) => {
     const { event, data } = JSON.parse(message.data);
     const listener = EVENT_LISTENERS[event];
@@ -10,27 +20,27 @@ const JsonWebSocket = (wsUrl) => {
     else console.error("NO WS EVENT LISTENER FOR", event);
   };
 
-  const setEventListener = (event, listener) => {
+  public.setEventListener = (event, listener) => {
     if (listener) EVENT_LISTENERS[event] = listener;
     else delete EVENT_LISTENERS[event];
   };
 
-  const send = (event, data) => {
+  public.send = (event, data) => {
     ws.send(JSON.stringify(event, data));
   };
 
-  const sendSync = async (event, data) => {
+  public.sendSync = async (event, data) => {
     const prevListener = EVENT_LISTENERS[event];
     let resolvePromise;
     const response = new Promise((resolve) => (resolvePromise = resolve));
-    setEventListener(event, (data) => {
+    public.setEventListener(event, (data) => {
       resolvePromise(data);
     });
     ws.send(JSON.stringify({ event, data }));
     const resData = await response;
-    setEventListener(prevListener);
+    public.setEventListener(prevListener);
     return resData;
   };
 
-  return { setEventListener, send, sendSync };
+  return public;
 };
