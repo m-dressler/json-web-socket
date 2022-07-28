946db7e0a8eaec84ca181c93caee9154f933a9da
Mauritz <mdressler@prodport.com>
2022-07-28 14:20:53 +0200

    Initial Version

---
+++ JsonWebSocket.js
@@ -0,0 +1,36 @@
+const JsonWebSocket = (url) => {
+  const ws = new WebSocket(wsUrl);
+
+  const EVENT_LISTENERS = {};
+
+  ws.onmessage = (message) => {
+    const { event, data } = JSON.parse(message.data);
+    const listener = EVENT_LISTENERS[event];
+    if (listener) listener(data);
+    else console.error("NO WS EVENT LISTENER FOR", event);
+  };
+
+  const setEventListener = (event, listener) => {
+    if (listener) EVENT_LISTENERS[event] = listener;
+    else delete EVENT_LISTENERS[event];
+  };
+
+  const send = (event, data) => {
+    ws.send(JSON.stringify(event, data));
+  };
+
+  const sendSync = async (event, data) => {
+    const prevListener = EVENT_LISTENERS[event];
+    let resolvePromise;
+    const response = new Promise((resolve) => (resolvePromise = resolve));
+    setEventListener(event, (data) => {
+      resolvePromise(data);
+    });
+    ws.send(JSON.stringify({ event, data }));
+    const resData = await response;
+    setEventListener(prevListener);
+    return resData;
+  };
+
+  return { setEventListener, send, sendSync };
+};
