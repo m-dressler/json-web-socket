174f065fde9931574371e43637758dc691c01cbb
Mauritz <mdressler@prodport.com>
2022-09-20 19:29:51 +0200

    Documentation | Browser->JsonWebSocket

--- browser/JsonWebSocket.js
+++ browser/JsonWebSocket.js
@@ -1,17 +1,28 @@
-export const JsonWebSocket = (wsUrl) => {
-  const returnObject = {};
+
+/**
+ * Creates a new WebSocket client
+ *
+ * @param {string} wsUrl The ws:// or wss:// URL to connect to the WebSocket
+ */
+ const JsonWebSocket = (wsUrl) => {
+  const result = {};
+  /** @type {0|1|2|3} CONNECTING | CONNECTED | CLOSING | CLOSED */
+  result.readyState = WebSocket.CONNECTING;
+  /** @type {()=>void|undefined} */
+  result.onclose = undefined;
+  /** @type {()=>void|undefined} */
+  result.onopen = undefined;
 
   const EVENT_LISTENERS = {};
 
   const ws = new WebSocket(wsUrl);
   ws.onopen = () => {
-    if (returnObject.onopen) returnObject.onopen();
+    result.readyState = WebSocket.OPEN;
+    if (result.onopen) result.onopen();
   };
   ws.onclose = () => {
-    if (returnObject.onclose) returnObject.onclose();
-  };
-  ws.onerror = () => {
-    if (returnObject.onerror) returnObject.onerror();
+    result.readyState = WebSocket.CLOSED;
+    if (result.onclose) result.onclose();
   };
   ws.onmessage = (message) => {
     const { event, data } = JSON.parse(message.data);
@@ -20,29 +31,40 @@ export const JsonWebSocket = (wsUrl) => {
     else console.error("NO WS EVENT LISTENER FOR", event);
   };
 
-  returnObject.on = (event, listener) => {
+  result.on = (event, listener) => {
     if (listener) EVENT_LISTENERS[event] = listener;
     else delete EVENT_LISTENERS[event];
   };
 
-  returnObject.send = (event, data) => {
+  /**
+   * Sends a JsonWebSocket message to the connection
+   *
+   * @param {string} event
+   * @param {any} data
+   */
+  result.send = (event, data) => {
     ws.send(JSON.stringify({ event, data }));
   };
 
-  returnObject.sendSync = async (event, data) => {
+  /**
+   * Sends a JsonWebSocket message to the connection and allows to await the response to the same message
+   *
+   * @param {string} event
+   * @param {any} data
+   * @returns {Promise<any>}
+   */
+  result.sendSync = async (event, data) => {
     const prevListener = EVENT_LISTENERS[event];
     let resolvePromise;
     const response = new Promise((resolve) => (resolvePromise = resolve));
-    returnObject.on(event, (data) => {
+    result.on(event, (data) => {
       resolvePromise(data);
     });
     ws.send(JSON.stringify({ event, data }));
     const resData = await response;
-    returnObject.on(prevListener);
+    result.on(prevListener);
     return resData;
   };
 
-  return returnObject;
+  return result;
 };
-
-export default JsonWebSocket;
