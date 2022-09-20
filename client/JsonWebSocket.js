dc426524b5ef0db932190c9f010a2c672b117000
Mauritz <mdressler@prodport.com>
2022-09-20 19:28:20 +0200

    ++Documentation | JsonWebSocket

--- client/JsonWebSocket.js
+++ client/JsonWebSocket.js
@@ -1,35 +1,29 @@
 const { WebSocket } = require("ws");
 
 /**
- * @typedef {Object} JsonWebSocket
- * @property {(0|1|2|3)} readyState CONNECTING | CONNECTED | CLOSING | CLOSED
- * @property {function():void} onopen
- * @property {function():void} onclose
- * @property {function(string, function(object):void):void} on Add an event listener to a JsonWebSocket event
- * @property {function(string, object):void} send Sends a JsonWebSocket message to the connection
- * @property {function(string, object):Promise<object>} sendSync Sends a JsonWebSocket message to the connection and allows to await the response to the same message
- */
-
-/**
+ * Creates a new WebSocket client
  *
  * @param {string} wsUrl The ws:// or wss:// URL to connect to the WebSocket
- * @returns {JsonWebSocket} The JsonWebSocket
  */
 const JsonWebSocket = (wsUrl) => {
-  const returnObject = {
-    readyState: WebSocket.CONNECTING,
-  };
+  const result = {};
+  /** @type {0|1|2|3} CONNECTING | CONNECTED | CLOSING | CLOSED */
+  result.readyState = WebSocket.CONNECTING;
+  /** @type {()=>void|undefined} */
+  result.onclose = undefined;
+  /** @type {()=>void|undefined} */
+  result.onopen = undefined;
 
   const EVENT_LISTENERS = {};
 
   const ws = new WebSocket(wsUrl);
   ws.onopen = () => {
-    returnObject.readyState = WebSocket.OPEN;
-    if (returnObject.onopen) returnObject.onopen();
+    result.readyState = WebSocket.OPEN;
+    if (result.onopen) result.onopen();
   };
   ws.onclose = () => {
-    returnObject.readyState = WebSocket.CLOSED;
-    if (returnObject.onclose) returnObject.onclose();
+    result.readyState = WebSocket.CLOSED;
+    if (result.onclose) result.onclose();
   };
   ws.onmessage = (message) => {
     const { event, data } = JSON.parse(message.data);
@@ -38,29 +32,42 @@ const JsonWebSocket = (wsUrl) => {
     else console.error("NO WS EVENT LISTENER FOR", event);
   };
 
-  returnObject.on = (event, listener) => {
+  result.on = (event, listener) => {
     if (listener) EVENT_LISTENERS[event] = listener;
     else delete EVENT_LISTENERS[event];
   };
 
-  returnObject.send = (event, data) => {
+  /**
+   * Sends a JsonWebSocket message to the connection
+   *
+   * @param {string} event
+   * @param {any} data
+   */
+  result.send = (event, data) => {
     ws.send(JSON.stringify({ event, data }));
   };
 
-  returnObject.sendSync = async (event, data) => {
+  /**
+   * Sends a JsonWebSocket message to the connection and allows to await the response to the same message
+   *
+   * @param {string} event
+   * @param {any} data
+   * @returns {Promise<any>}
+   */
+  result.sendSync = async (event, data) => {
     const prevListener = EVENT_LISTENERS[event];
     let resolvePromise;
     const response = new Promise((resolve) => (resolvePromise = resolve));
-    returnObject.on(event, (data) => {
+    result.on(event, (data) => {
       resolvePromise(data);
     });
     ws.send(JSON.stringify({ event, data }));
     const resData = await response;
-    returnObject.on(prevListener);
+    result.on(prevListener);
     return resData;
   };
 
-  return returnObject;
+  return result;
 };
 
 module.exports = JsonWebSocket;
