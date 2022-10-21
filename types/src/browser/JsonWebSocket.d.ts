bb51c8caa14e480e1028f8559bdada485db57290
Mauritz <mdressler@prodport.com>
2022-10-21 12:44:00 +0200

    Refactor | Moved code to /src/ folder

--- types/src/browser/JsonWebSocket.d.ts
+++ types/src/browser/JsonWebSocket.d.ts
@@ -1,5 +1,5 @@
 export function JsonWebSocket(url?: string | undefined, params?: {
     reconnectTime?: number | undefined;
     connectTimeout?: number | undefined;
-} | undefined): import('../client/JsonWebSocket.js').JsonWebSocket;
+} | undefined): any;
 export default JsonWebSocket;
